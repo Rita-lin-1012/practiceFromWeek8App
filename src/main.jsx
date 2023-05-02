24f424ab30e4e9245ff29f7c0ab115bf9e065830
Rita-lin-1012 <rita10778@gmail.com>
2023-05-03 01:18:46 +0800

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+/* week8 */
+// import App from './App'
+import './index.css'
+/* week9 */
+// import App from './week9/App-withCard'
+// import App from './week9/App-withCard-and-NavbarCustom'
+import App from './week9/App-customCard'
+// import App from "./week9/App-stamp"
+// import {Stamp} from './components/Stamp4'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    {/* <Stamp/> */}
+  </React.StrictMode>,
+)
---
+++ src/week9/App-customCard.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import {Main} from "../components/Layout2";
+import { NavbarCustom, NavbarItem } from "../components/NavbarCustom";
+import './App.css'
+import PlayIcon from '@mui/icons-material/PlayArrow';
+import NextIcon from '@mui/icons-material/FastForward';
+import PreviousIcon from '@mui/icons-material/FastRewind';
+// import Card from '../components/Card';
+// design my own card
+import Card from '../components/Card2'; 
+
+export default function App() {
+    const user = {
+        name: "王小明",
+        grade: "大一",
+        imgSrc: "https://cms-carrier.ntpu.edu.tw/uploads/8d03554e2e3a4f8dacbcad536cf70bfc.jpg",
+        wish: "New beginnings, new chapters; new life and new endeavors. My friend, I am really proud of you! It feels amazing to share this happiness and excitement with you. Congratulations on graduating!"
+    }
+    const user2 = {
+        name: "Henry",
+        grade: "Senior",
+        imgSrc: "https://cc.ntpu.edu.tw/images/2018/12/04/image.jpg",
+        wish: "Happy graduation. I bless you with all that you need to earn many more achievements and facts in life ahead. Congratulations and well done. We're so happy that you've achieved this great milestone- Wishing you great success with your dreams and career. It's official: you're now too cool for school"
+    
+    }
+
+    return (
+        <div className="App">
+            <Main>
+                <Card user={user2}/>
+            </Main>
+            <NavbarCustom>
+                <NavbarItem>
+                    <PreviousIcon/>
+                </NavbarItem>
+                <NavbarItem>
+                    <PlayIcon/>
+                </NavbarItem>
+                <NavbarItem>
+                    <NextIcon/>
+                </NavbarItem>
+            </NavbarCustom>
+        </div>
+    )
+}
