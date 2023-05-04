0c6571814618a96ab9b8bbf6f11042b9e1e836ff
Rita-lin-1012 <rita10778@gmail.com>
2023-05-04 18:02:57 +0800

    CREATE DOCS

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/practicefromweek8app/",
+  build:{
+    outDir:'docs'
+  },
   plugins: [react()],
 })
